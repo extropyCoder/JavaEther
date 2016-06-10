29ea08f30b462f3d925a2d179a97c3d723c3ac7a
Laurence <laurence@extropy.io>
2016-06-10 07:49:19 +0100

    re package

--- src/main/java/io/extropy/rest/MyRestController.java
+++ src/main/java/io/extropy/rest/MyRestController.java
@@ -1,7 +1,7 @@
-package com.ethercamp.starter.rest;
+package io.extropy.rest;
 
 
-import com.ethercamp.starter.ethereum.EthereumBean;
+import io.extropy.ethereum.EthereumBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
